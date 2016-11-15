f83d47c9ff4876adae2f358a5145333b13398951
Olivier Le Merdy <olemerdy@financeactive.com>
2016-11-15 16:25:07 +0100

    Transaction Repository is dynamic

--- src/app/account/organization/portfolio/transaction-repository.service.ts
+++ src/app/account/organization/portfolio/transaction-repository.service.ts
@@ -4,18 +4,30 @@ import {Transaction} from './transaction';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {Filter} from '../filter';
+import {Http} from '@angular/http';
 
 @Injectable()
 export class TransactionRepository {
 
-  private transactions: Transaction[] = [
-    {id: 123, name: 'Loan ABC'},
-    {id: 345, name: 'Swap DEF'},
-    {id: 789, name: 'Cap GHI'}
-  ];
+  constructor(private http: Http) {
+  }
 
   findAll(account: Account, organization: Organization, filter: Filter): Observable<Transaction[]> {
-//  console.log(`Find all transactions for account: ${JSON.stringify(account)}, organization: ${JSON.stringify(organization)} and filter: ${JSON.stringify(filter)}`);
-    return Observable.of(this.transactions).delay(100);
+    if (account && organization) {
+      return this.http.get('./assets/data.json')
+        .map(res => res.json().accounts)
+        .map(accs => accs ? accs.find(acc => acc.id === account.id) : undefined)
+        .map(acc => {
+          let txs = acc && acc.transactions ? acc.transactions : [];
+          let orgs = acc && acc.organizations ? acc.organizations : [];
+          let org = orgs.find(org => org.id === organization.id);
+          let fils = org && org.filters ? org.filters : [];
+          let fil = fils.find(fil => filter && fil.id === filter.id);
+          let orgTxIds: any[] = org && org.transaction_ids ? org.transaction_ids : [];
+          return txs.filter(tx => orgTxIds.indexOf(tx.id) >= 0);
+        })
+        .map(txs => txs as Transaction[])
+    }
+    return Observable.of([]);
   }
 }
