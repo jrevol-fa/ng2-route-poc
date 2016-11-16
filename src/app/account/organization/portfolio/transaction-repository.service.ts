042ea80c9d1fbc5fa85dbe0aa2940a33312bb85e
Olivier Le Merdy <olemerdy@financeactive.com>
2016-11-16 11:53:36 +0100

    Implement filter restriction of portfolio

--- src/app/account/organization/portfolio/transaction-repository.service.ts
+++ src/app/account/organization/portfolio/transaction-repository.service.ts
@@ -21,10 +21,13 @@ export class TransactionRepository {
           let txs = acc && acc.transactions ? acc.transactions : [];
           let orgs = acc && acc.organizations ? acc.organizations : [];
           let org = orgs.find(org => org.id === organization.id);
+          let orgTxIds: any[]|undefined = org && org.transaction_ids ? org.transaction_ids : undefined;
           let fils = org && org.filters ? org.filters : [];
           let fil = fils.find(fil => filter && fil.id === filter.id);
-          let orgTxIds: any[] = org && org.transaction_ids ? org.transaction_ids : [];
-          return txs.filter(tx => orgTxIds.indexOf(tx.id) >= 0);
+          let filTxIds: any[]|undefined = fil && fil.transaction_ids ? fil.transaction_ids : undefined;
+          return txs
+            .filter(tx => orgTxIds === undefined || orgTxIds.indexOf(tx.id) >= 0)
+            .filter(tx => filTxIds === undefined || filTxIds.indexOf(tx.id) >= 0);
         })
         .map(txs => txs as Transaction[])
     }
